729063088f4ff75db22fa5f518b704896f65be00
vinipena <vacpena@gmail.com>
2022-02-03 10:37:01 -0300

    Comitando os arquivos completos do curso

---
+++ gatitobook/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { RodapeModule } from './componentes/rodape/rodape.module';
+import { CabecalhoModule } from './componentes/cabecalho/cabecalho.module';
+import { NgModule } from '@angular/core';
+import { HomeModule } from './home/home.module';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { AutenticacaoModule } from './autenticacao/autenticacao.module';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HomeModule,
+    HttpClientModule,
+    CabecalhoModule,
+    RodapeModule,
+    AutenticacaoModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
